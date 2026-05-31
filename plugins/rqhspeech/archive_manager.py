5e5812424caac91013e0ab3338a6d85dde2bcd4f
Ruqiehou <2654278608@qq.com>
2026-05-31 10:20:26 +0800

    2

--- plugins/rqhspeech/archive_manager.py
+++ plugins/rqhspeech/archive_manager.py
@@ -15,7 +15,7 @@ from typing import List, Dict, Any, Optional, Tuple
 if __name__ == "__main__":
     import sys
     sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
-    from plugins.statistics.speech_config import SpeechConfig
+    from plugins.rqhspeech.speech_config import SpeechConfig
 else:
     from .speech_config import SpeechConfig
 
